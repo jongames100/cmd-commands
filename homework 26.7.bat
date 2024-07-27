fa7f2dbbdd3a0cbe8aab9a0121dc6d3c85920864
jongames100 <jongames100@gmail.com>
2024-07-27 20:04:47 +0300

    Update homework 26.7.bat

--- homework 26.7.bat
+++ homework 26.7.bat
@@ -1,12 +1,8 @@
-
 mkdir new_directory
-
 
 netstat > new_directory\netstat_output.txt
 
-
-findstr ":80" new_directory\netstat_output.txt
-
+findstr /R "[:][0]*80\>" new_directory\netstat_output.txt
 
 del new_directory\netstat_output.txt
 
